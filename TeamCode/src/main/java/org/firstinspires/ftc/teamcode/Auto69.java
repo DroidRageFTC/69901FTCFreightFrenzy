715f46570b357a5e83e3994be6f770b30456c52f
Your Name <you@example.com>
2021-12-11 09:35:24 -0600

    Auton changes value of how far it goes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto69.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto69.java
@@ -116,8 +116,8 @@ public class Auto69 extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  5,  5, 3.0);  // S1: Forward 47 Inches with 5 Sec timeout
-        encoderDrive(TURN_SPEED,   5, -6, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED,  6,  6, 3.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED,   5, -5, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
         encoderDrive(DRIVE_SPEED, 15, 15, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
 
 
